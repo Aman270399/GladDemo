793393d00bbaa08f93a28263ee53b3c35e1342e0
mohit <agrawalmohit603@gmail.com>
2020-09-25 17:53:12 +0530

    searchpage
    searchpage

--- Frontend/src/app/app.module.ts
+++ Frontend/src/app/app.module.ts
@@ -1,6 +1,6 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
+import { ReactiveFormsModule, FormsModule } from '@angular/forms';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HomeComponent } from './components/home/home.component';
@@ -12,7 +12,9 @@ import { HomeComponent } from './components/home/home.component';
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    FormsModule,
+    ReactiveFormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
--- Frontend/src/app/components/home/home.component.ts
+++ Frontend/src/app/components/home/home.component.ts
@@ -1,4 +1,8 @@
 import { Component, OnInit } from '@angular/core';
+import { FormGroup, FormBuilder } from '@angular/forms';
+import { AirportlistService } from 'src/app/services/airportlist.service';
+
+import {airportlist} from 'src/app/models/airportlist';
 
 @Component({
   selector: 'app-home',
@@ -6,10 +10,31 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./home.component.css']
 })
 export class HomeComponent implements OnInit {
-
-  constructor() { }
+  airports:airportlist[];
+  SearchForm:FormGroup;
+  cities:any=['Delhi','Mumbai'];
+  constructor(private formbulider: FormBuilder,private airportservice:AirportlistService) { }
 
   ngOnInit(): void {
+    this.SearchForm = this.formbulider.group({    
+      source: [''],    
+      destination: [''],    
+      departdate: [''],   
+      returndate: [''], 
+      adultpassengercount:[''],
+      childpassengercount:[''],
+      infantpassengercount:[''],
+    }); 
+    this.airportservice.getAllUser()._subscribe<airportlist> (data=>{this.airports=data;});
   }
+  
+  onSubmit(form){
+    console.log(form);  
+    }
 
+   /*  textBoxDisabled = true;
+    toggle(){
+      this.textBoxDisabled = !this.textBoxDisabled;
+    }
+ */
 }
