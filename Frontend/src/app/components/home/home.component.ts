8d9553a7a1f51bfdbd84528d3988fdde48082d60
arshanad <arhandas1998ad@gmail.com>
2020-09-25 19:34:16 +0530

    Reordered header, home

--- Frontend/src/app/components/home/home.component.ts
+++ Frontend/src/app/components/home/home.component.ts
@@ -1,7 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { FormGroup, FormBuilder } from '@angular/forms';
 import { AirportlistService } from 'src/app/services/airportlist.service';
-
 import {airportlist} from 'src/app/models/airportlist';
 
 @Component({
@@ -13,7 +12,7 @@ export class HomeComponent implements OnInit {
   airports:airportlist[];
   SearchForm:FormGroup;
   cities:any=['Delhi','Mumbai'];
-  constructor(private formbulider: FormBuilder,private airportservice:AirportlistService) { }
+  constructor(private formbulider: FormBuilder) { }
 
   ngOnInit(): void {
     this.SearchForm = this.formbulider.group({    
