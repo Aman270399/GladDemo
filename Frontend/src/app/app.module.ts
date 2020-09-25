5528675720dec736a6edd06bef5261dc3ccbb271
arshanad <arhandas1998ad@gmail.com>
2020-09-25 18:21:08 +0530

    app module changes

--- Frontend/src/app/app.module.ts
+++ Frontend/src/app/app.module.ts
@@ -1,11 +1,6 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-<<<<<<< Updated upstream
 import { ReactiveFormsModule, FormsModule } from '@angular/forms';
-=======
-import { AngularMaterialModule } from './angular-material.module';
-import { FormsModule } from '@angular/forms';
->>>>>>> Stashed changes
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HomeComponent } from './components/home/home.component';
@@ -23,14 +18,11 @@ import { FooterComponent } from './shared/footer/footer.component';
   imports: [
     BrowserModule,
     AppRoutingModule,
-<<<<<<< Updated upstream
     FormsModule,
-    ReactiveFormsModule
-=======
-    AngularMaterialModule,
+    ReactiveFormsModule,
+    // AngularMaterialModule,
     FormsModule,
     BrowserAnimationsModule
->>>>>>> Stashed changes
   ],
   providers: [],
   bootstrap: [AppComponent]
