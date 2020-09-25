ef31470f4b6bd44440976d56b65e19709df456b9
khushimis <khushimishra1104@gmail.com>
2020-09-25 22:51:48 +0530

    UserLogin Component
    User login component in frontend

--- Frontend/src/app/app.module.ts
+++ Frontend/src/app/app.module.ts
@@ -9,6 +9,7 @@ import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { HeaderComponent } from './shared/header/header.component';
 import { FooterComponent } from './shared/footer/footer.component';
 import { AdminloginComponent } from './components/adminlogin/adminlogin.component';
+import { UserloginComponent } from './components/userlogin/userlogin.component';
 
 
 
@@ -20,7 +21,9 @@ import { AdminloginComponent } from './components/adminlogin/adminlogin.componen
     HeaderComponent,
     FooterComponent,
 
-    AdminloginComponent
+    AdminloginComponent,
+
+    UserloginComponent
 
   ],
   imports: [
