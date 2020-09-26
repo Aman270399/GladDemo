355d2cd63526654001997e9901b0026a1a230adc
mohit <agrawalmohit603@gmail.com>
2020-09-26 21:06:08 +0530

    home

--- Frontend/src/app/components/home/home.component.ts
+++ Frontend/src/app/components/home/home.component.ts
@@ -1,10 +1,9 @@
 import { Component, OnInit } from '@angular/core';
-
 import { FormGroup, FormBuilder, Validators } from '@angular/forms';
 import { AirportlistService } from 'src/app/services/airportlist.service';
 import {airportlist} from 'src/app/models/airportlist';
-
 import {NgForm} from '@angular/forms';
+import { Router } from '@angular/router';
 
 
 @Component({
@@ -17,33 +16,33 @@ export class HomeComponent implements OnInit {
   SearchForm:FormGroup;
   cities:any[];
   data:any;
-  constructor(private formbulider: FormBuilder,private airportservice:AirportlistService) { }
+  constructor(private formbulider: FormBuilder,private airportservice:AirportlistService,private router:Router) { }
 
   ngOnInit(): void {
     this.airportservice.getallairports().subscribe(data=>{
       this.airports=data;
       console.log(this.airports); });
    
-    this.SearchForm = this.formbulider.group({    
+    this.SearchForm = this.formbulider.group({  
+      flighttype:['',Validators.required], 
       source: ['',Validators.required],    
-      destination: ['',Validators.required],    
+      destination: ['' ,Validators.required],    
       departdate: ['',Validators.required],   
       returndate: ['',Validators.required], 
       adultpassengercount:['',Validators.required],
       childpassengercount:['',Validators.required],
       infantpassengercount:['',Validators.required],
+      seatclass:['',Validators.required],
     }); 
     
   }
-  disabled:any;
-  EnableDisableTextBox() {
-    this.disabled=true;
-    }
+  returndatedisabled:boolean;
 
   submitted:any;
   onSubmit(form){
     this.submitted=true;
     console.log(form);  
+    this.router.navigate(['flightselect',form.source,form.destination,form.departdate,form.returndate,form.adultpassengercount,form.childpassengercount,form.infantpassengercount],);
     }
 
 
