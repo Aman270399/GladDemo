6160f681d9cbc6b9a9bd0679c096a3d556729636
arshanad <arhandas1998ad@gmail.com>
2020-09-26 12:56:42 +0530

    Add flightrepository

--- Backend/Airlines_WebApp/Repository/FlightRepository.cs
+++ Backend/Airlines_WebApp/Repository/FlightRepository.cs
@@ -5,7 +5,38 @@ using System.Web;
 
 namespace Airlines_WebApp.Repository
 {
-    public class FlightRepository
+    public class FlightRepository : IDataRepository<Flight>
     {
+        public readonly GladiatorProjectEntities1 projectContext;
+        public FlightRepository(GladiatorProjectEntities1 projectDb)
+        {
+            projectContext = projectDb;
+        }
+        public IEnumerable<Flight> GetAll()
+        {
+            return projectContext.Flights.ToList();
+            // throw new NotImplementedException();
+        }
+        public void Add(Flight newFlight)
+        {
+            projectContext.Flights.Add(newFlight);
+            projectContext.SaveChanges();
+        }
+
+        public Flight Get(int id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Update(Flight dbEntity)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Delete(int entity)
+        {
+            throw new NotImplementedException();
+        }
     }
+}
 }
