788e97c9a4c9f612d2fe7b8a3f32b38e3a791c58
aman270399 <aman27399@gmail.com>
2020-09-28 01:48:28 +0530

    Frontend

--- Frontend/src/app/services/auth.service.ts
+++ Frontend/src/app/services/auth.service.ts
@@ -6,11 +6,13 @@ import { Injectable } from '@angular/core';
 })
 export class AuthService {
   API_URI = 'https://localhost:44374/api/accounts';
-  
+  isAuthenticatedUser = false;
+  isAuthenticatedAdmin = false;
   constructor(private http: HttpClient) { }
   doLogin(data) {
     console.log(data);
     return this.http.post(this.API_URI+"/userlogin", data);
+    
   }
   doAdminLogin(data) {
     console.log(data);
