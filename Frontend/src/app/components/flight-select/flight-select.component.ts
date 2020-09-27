11c56fc5112413e4ea147714699901c8f8feac26
arshanad <arhandas1998ad@gmail.com>
2020-09-27 21:31:02 +0530

    changes to flight_select

--- Frontend/src/app/components/flight-select/flight-select.component.ts
+++ Frontend/src/app/components/flight-select/flight-select.component.ts
@@ -8,9 +8,9 @@ import { Component, OnInit } from '@angular/core';
 export class FlightSelectComponent implements OnInit {
 
   constructor() { }
-
+  isRound:boolean;
   ngOnInit(): void {
+    this.isRound=localStorage.getItem("type")=="roundtrip"?true:false;
   }
- type:any=localStorage.getItem("type")=="roundtrip"?true:false;
 
 }
