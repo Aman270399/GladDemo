8422842d22604d11c77adb77c43c825fac268a97
mohit <agrawalmohit603@gmail.com>
2020-09-28 22:54:22 +0530

    passenger

--- Frontend/src/app/components/passengerdetails/passengerdetails.component.ts
+++ Frontend/src/app/components/passengerdetails/passengerdetails.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { FormGroup } from '@angular/forms';
 
 @Component({
   selector: 'app-passengerdetails',
@@ -6,10 +7,31 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./passengerdetails.component.css']
 })
 export class PassengerdetailsComponent implements OnInit {
-
+adultpassengercount:number;
+childpassengercount:number;
+infantpassengercount:number;
+dynamicForm: FormGroup;
+    submitted = false;
   constructor() { }
 
   ngOnInit(): void {
+    this.adultpassengercount=+localStorage.getItem('adultpassengercount');
+    this.childpassengercount=+localStorage.getItem('childpassengercount');
+    this.infantpassengercount=+localStorage.getItem('infantpassengercount');
+    console.log(this.childpassengercount);
+    console.log(this.adultpassengercount);
   }
 
+  createRange(number){
+    var items: number[] = [];
+    for(var i = 1; i <= number; i++){
+       items.push(i);
+    }
+    return items;
+ 
+  }
+  onSubmit() {}
+  
+
+    
 }
