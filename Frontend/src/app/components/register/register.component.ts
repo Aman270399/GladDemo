6d2142e469537396641eed1ca6fcdf72cb7ad6ef
arshanad <arhandas1998ad@gmail.com>
2020-09-28 17:10:24 +0530

    register validations

--- Frontend/src/app/components/register/register.component.ts
+++ Frontend/src/app/components/register/register.component.ts
@@ -21,17 +21,17 @@ export class RegisterComponent implements OnInit {
       LastName:["",[Validators.required,Validators.pattern('[a-zA-Z]+')]],
       UserEmailId:["",[Validators.required,Validators.email]],
       Age : ["",[Validators.required]],
-      MobileNumber : ["",[Validators.required,Validators.pattern('[0-9]{10}')]],
-      Password : ["",[Validators.required]],
+      MobileNumber : ["",[Validators.required,Validators.pattern('[7-9][0-9]{9}')]],
+      Password : ["",[Validators.required, Validators.minLength(8),Validators.pattern('(?=.*[a-z])(?=.*[A-Z])(?=.*[0-9])(?=.*[$@$!%*?&])[A-Za-z\d$@$!%*?&].{8,}')]],
       DateOfBirth:["",[Validators.required]],
       confirmPass:["",[Validators.required]]
-    })
+    }, { validator: this.checkPasswords })
   }
-  // checkPasswords(group: FormGroup) { // here we have the 'passwords' group
-  // let pass = group.get('Password').value;
-  // let confirmPass = group.get('confirmPass').value;
-  // return pass === confirmPass ? null : { notSame: true }     
-  // }
+  checkPasswords(group: FormGroup) { // here we have the 'passwords' group
+  let pass = group.get('Password').value;
+  let confirmPass = group.get('confirmPass').value;
+   return pass === confirmPass ? null : { notSame: true }     
+  }
   onSubmit(form : User){
     console.log(form);
     
