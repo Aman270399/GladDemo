73590939c41ec54141b219fd1a060d40d6c1f4e0
khushimis <khushimishra1104@gmail.com>
2020-09-29 16:45:44 +0530

    reset password

--- Frontend/src/app/services/user.service.ts
+++ Frontend/src/app/services/user.service.ts
@@ -18,4 +18,20 @@ export class UserService {
   addUser(user){
     return this.http.post<any>(this.baseUrl,JSON.stringify(user),this.httpOptions);
   }
+  getUserById(id:string){
+    return this.http.get<User>(this.baseUrl+'id',this.httpOptions);
+  }
+  userUpdate(user:User,data){
+    const newDetail={
+      "Password": user.Password,
+      "UserEmailId":user.UserEmailId,
+      "Title":user.Title,
+      "Age":user.Age,
+      "MobileNumber":user.MobileNumber,
+      "FirstName": user.FirstName,
+      "LastName": user.LastName,
+      "DateOFBirth":user.DateOFBirth
+    }
+    return this.http.put(this.baseUrl+'/'+user.UserEmailId, JSON.stringify(newDetail),this.httpOptions);
+  }
 }
