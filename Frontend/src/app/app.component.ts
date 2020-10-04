1fffcd9a2e79148d5dab5daacddcf69b54c5e7ed
arshanad <arhandas1998ad@gmail.com>
2020-10-04 07:48:07 +0530

    added carousel to background image fade on transition

--- Frontend/src/app/app.component.ts
+++ Frontend/src/app/app.component.ts
@@ -1,10 +1,27 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import * as $ from 'jquery'
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
+export class AppComponent implements OnInit{
   title = 'Domestic Glide';
+  constructor()
+  {
+    
+    
+
+    // Initialize carousel
+  }
+  ngOnInit(): void {
+    var $myCarousel = $('#mycarousel');
+    $myCarousel.carousel(
+      {
+        interval:1000,pause: 'none'
+      }
+    );
+  }
 }
+
